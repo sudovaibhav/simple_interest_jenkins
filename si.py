40d29b49bc1560d6e9f80be5a21348a2e02b37b6
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-11-28 21:35:45 +0530

    added program

---
+++ si.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) == 4:
+    script_name = sys.argv[0]
+    principal = sys.argv[1]
+    rate_of_interest = sys.argv[2]
+    time = sys.argv[3]
+else:
+    script_name = sys.argv[0]
+    principal = 10000
+    rate_of_interest = 10
+    time = 3
+    print("Invalid input using default values.")
+    simple_interest = (principal * time * rate_of_interest) / 100
+    print(f"Principal amount: {principal}\nRate of interest: {rate_of_interest}\nTime in years: {time}")
+    
